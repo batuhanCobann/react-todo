3e13414a11e4a7b4bfc03657460b9c35cf0a50cd
Batuhan Çoban <batuhancoban@Batuhan-MacBook-Pro.local>
2024-06-28 01:02:03 +0300

    addbtn function ile yazılan todolar ekrana basıldı

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
 import { useState } from 'react'
 import './App.css'
 
+let lastId = 1;
+
+function lastNewId(){
+  return lastId ++;
+}
+
 function App() {
 
-  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
 
-  console.log(todo);
+  const [newTodoTitle, setNewTodoTitle] = useState([])
 
+  function addTodo (){
+    const newTodoItem = {
+      id: lastNewId(),
+      title: newTodoTitle,
+      copleted: false
+    };
+
+    setTodos([ ...todos, newTodoItem ])
+  }
+
+  console.log(todos);
   return (
     <>
-      <input type="text" placeholder='Lütfen Bir Görev Giriniz...' onChange={(e) => setTodo(e.target.value)} />
+      <input 
+      type="text" 
+      placeholder='Lütfen Bir Görev Giriniz...' 
+      onChange={(e) => setNewTodoTitle(e.target.value)} />
+
+      <button onClick={addTodo}>Add Todo</button>
+      {todos.map(todo => (
+        <p className={todo.copleted ? "okey-todo" : "no-todo"} key={todo.id}>{todo.title} <button>Delete</button></p>
+      ))}
     </>
   )
 }
