65400215384d03e8044283e7255aec687fc33d4e
Batuhan Çoban <batuhancoban@Batuhan-MacBook-Pro.local>
2024-06-27 23:14:19 +0300

    todo için bir input alanıo oluşturuldu ve inpuda girilen degerler onChange ile alındı

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
+  const [todo, setTodo] = useState("");
+
+  console.log(todo);
+
   return (
     <>
-      
+      <input type="text" placeholder='Lütfen Bir Görev Giriniz...' onChange={(e) => setTodo(e.target.value)} />
     </>
   )
 }
